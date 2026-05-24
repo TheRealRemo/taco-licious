c24b39c2dcb21a772b01872421b47f05cb6fbf9f
TheRealRemo <timothyprice2001@gmail.com>
2026-05-23 23:55:21 -0400

    Updated 'Taco' class for attributes, constructor, getters and setters

--- src/main/java/com/pluralsight/tacoshop/model/Taco.java
+++ src/main/java/com/pluralsight/tacoshop/model/Taco.java
@@ -2,7 +2,41 @@ package com.pluralsight.tacoshop.model;
 
 import com.pluralsight.tacoshop.utils.IPriceable;
 
+import java.util.ArrayList;
+
 public class Taco implements IPriceable {
+    private String size;
+    private String shell;
+    private ArrayList<Topping> toppings;
+    private boolean isCoveredInSalsaAndQueso;
+
+    public Taco() {
+        this.toppings = new ArrayList<>();
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getShell() {
+        return shell;
+    }
+
+    public void setShell(String shell) {
+        this.shell = shell;
+    }
+
+    public boolean isCoveredInSalsaAndQueso() {
+        return isCoveredInSalsaAndQueso;
+    }
+
+    public void setCoveredInSalsaAndQueso(boolean coveredInSalsaAndQueso) {
+        isCoveredInSalsaAndQueso = coveredInSalsaAndQueso;
+    }
 
     @Override
     public double getPrice() {
