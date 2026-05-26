6a2776c5eca35eb1521e2b7a3037bf3ec6770f64
TheRealRemo <timothyprice2001@gmail.com>
2026-05-26 15:18:49 -0400

    Updated 'Order' class attributes and methods

--- src/main/java/com/pluralsight/tacoshop/model/Order.java
+++ src/main/java/com/pluralsight/tacoshop/model/Order.java
@@ -1,4 +1,29 @@
 package com.pluralsight.tacoshop.model;
 
-public class Order {
+import com.pluralsight.tacoshop.utils.IPriceable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order implements IPriceable {
+    private ArrayList<IPriceable> items;
+
+    public void addItem(IPriceable priceable) {
+        items.add(priceable);
+    }
+
+    public ArrayList<IPriceable> getItems() {
+        return items;
+    }
+    @Override
+    public double calculatePrice() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "items=" + items +
+                '}';
+    }
 }
