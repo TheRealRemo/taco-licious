d629fcff5a325afd92515cc1aca1e61c6393b107
TheRealRemo <timothyprice2001@gmail.com>
2026-05-26 09:54:31 -0400

    Updated 'Drink' class for attributes, constructor, getters and setters

--- src/main/java/com/pluralsight/tacoshop/model/Drink.java
+++ src/main/java/com/pluralsight/tacoshop/model/Drink.java
@@ -3,9 +3,32 @@ package com.pluralsight.tacoshop.model;
 import com.pluralsight.tacoshop.utils.IPriceable;
 
 public class Drink implements IPriceable {
+private String flavor;
+private String size;
+
+    public Drink(String flavor, String size) {
+        this.flavor = flavor;
+        this.size = size;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public String getSize() {
+        return size;
+    }
 
     @Override
     public double calculatePrice() {
         return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Drink{" +
+                "flavor='" + flavor + '\'' +
+                ", size='" + size + '\'' +
+                '}';
     }
 }
